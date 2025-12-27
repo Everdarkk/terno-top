9ff49db3f5aeee34124816596dcf8f08ab160ded
Everdarkk <alex.dorokhov.mail@gmail.com>
2025-12-27 11:39:47 +0200

    Minor changes to cards styles

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,46 +1,42 @@
 import { overpass, geologica } from '@/app/layout'
 import Image from "next/image"
 import styles from '../../styles/ProductCard.module.css'
-import { Product } from '../../lib/types/index'
-
+import { Product } from '../../lib/types'
 
 type Props = {
-    product: Product
+  product: Product
 }
 
-export default function ProductCard({product} : Props) {
-    const { title, materials, season, imgSrc } = product
+export default function ProductCard({ product }: Props) {
+  const { title, materials, season, imgSrc } = product
 
-    return (
-        <li>
-            <article className={styles.container}>
-                {/* IMAGE */}
-                <div className={styles.imageWrap}>
-                    <Image
-                        src={imgSrc}
-                        alt={title}
-                        width={500}
-                        height={500}
-                        className={styles.image}
-                    />
-                </div>
-                {/* DETAILS */}
-                <div className={styles.textWrap}>
-                    <h5 className={overpass.className}>
-                        {title}
-                    </h5>
-                    <p>
-                        {materials}
-                    </p>
-                    <p>
-                        {season}
-                    </p>
-                </div>
-                {/* DETAILS BUTTON */}
-                <button className={`${styles.btn} ${geologica.className}`}>
-                    Детальніше
-                </button>
-            </article>
-        </li>
-    )
+  return (
+    <li className={styles.listItem}>
+      <article className={styles.container}>
+        {/* IMAGE */}
+        <div className={styles.imageWrap}>
+          <Image
+            src={imgSrc}
+            alt={title}
+            width={500}
+            height={500}
+            className={styles.image}
+            draggable={false}
+          />
+        </div>
+
+        {/* DETAILS */}
+        <div className={styles.textWrap}>
+          <h5 className={overpass.className}>{title}</h5>
+          <p>{materials}</p>
+          <p>{season}</p>
+        </div>
+
+        {/* DETAILS BUTTON */}
+        <button className={`${styles.btn} ${geologica.className}`}>
+          Детальніше
+        </button>
+      </article>
+    </li>
+  )
 }
