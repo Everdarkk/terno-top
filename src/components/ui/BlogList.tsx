6da7e7f57ab0a93301e9fb6eabc49aa1b052fbca
Everdarkk <alex.dorokhov.mail@gmail.com>
2026-01-07 16:18:16 +0200

    Bounding to DB bucket where the components get images to blog posts

--- src/components/ui/BlogList.tsx
+++ src/components/ui/BlogList.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image"
 import styles from "../../styles/BlogList.module.css"
 import { createClient } from '@/lib/supabase/server'
+import Link from "next/link"
 
 export default async function BlogList() {
   const supabase = await createClient()
 
   const { data: articles, error } = await supabase
     .from('articles')
-    .select('id, title, created_at')
+    .select('id, title, created_at, imgUrl')
     .order('created_at', { ascending: false })
 
   if (error) {
@@ -14,20 +16,31 @@ export default async function BlogList() {
     return <p>Не вдалося завантажити статті</p>
   }
 
+
   return (
     <section className={styles.container}>
       <ul>
         {articles?.map(article => (
           <li key={article.id}>
-            <a href={`/blog/${article.id}`}>
+              <Image
+                src={article.imgUrl}
+                alt={article.title}
+                width={1000}
+                height={1000}
+                className={styles.image}
+              />
+
               <h2>{article.title}</h2>
+              
               <time>
                 {new Date(article.created_at).toLocaleDateString()}
               </time>
-              <button>
-                Читати
-              </button>
-            </a>
+
+              <Link href={`/blog/${article.id}`}>
+                <button>
+                  Читати
+                </button>
+              </Link>
           </li>
         ))}
       </ul>
