a84888277035e92c56bc674497930ba4f7d2f5a8
Everdarkk <alex.dorokhov.mail@gmail.com>
2026-01-08 12:46:54 +0200

    Added styles to BlogList component

--- src/components/ui/BlogList.tsx
+++ src/components/ui/BlogList.tsx
@@ -2,48 +2,63 @@ import Image from "next/image"
 import styles from "../../styles/BlogList.module.css"
 import { createClient } from '@/lib/supabase/server'
 import Link from "next/link"
+import { overpass } from "@/app/layout"
 
 export default async function BlogList() {
   const supabase = await createClient()
 
   const { data: articles, error } = await supabase
     .from('articles')
-    .select('id, title, created_at, imgUrl')
+    .select('id, title, article, created_at, imgUrl')
     .order('created_at', { ascending: false })
 
   if (error) {
     console.error(error)
-    return <p>Не вдалося завантажити статті</p>
+    return <p className={styles.error}>Не вдалося завантажити статті</p>
   }
 
 
   return (
     <section className={styles.container}>
-      <ul>
-        {articles?.map(article => (
-          <li key={article.id}>
-              <Image
-                src={article.imgUrl}
-                alt={article.title}
-                width={1000}
-                height={1000}
-                className={styles.image}
-              />
+      <div className={styles.content}>
+        {/* LIST OF ARTICLES */}
+        <ul className={styles.articlesList}>
+          {articles?.map(article => (
+            <li key={article.id} className={styles.card}>
+                {/* IMAGE */}
+                <div className={styles.imageWrap}>
+                  <Image
+                    src={article.imgUrl}
+                    alt={article.title}
+                    width={1000}
+                    height={1000}
+                    className={styles.image}
+                  />
+                </div>
+                
+                {/* TEXT WRAP */}
+                <div className={styles.textWrap}>
+                  {/* TITLE */}
+                  <h2 className={overpass.className}>{article.title}</h2>
+                  {/* TEXT */}
+                  <p>{article.article}</p>
+                </div>
+        
+                {/* TIME */}
+                <time className={styles.time}>
+                  {new Date(article.created_at).toLocaleDateString()}
+                </time>
 
-              <h2>{article.title}</h2>
-              
-              <time>
-                {new Date(article.created_at).toLocaleDateString()}
-              </time>
-
-              <Link href={`/blog/${article.id}`}>
-                <button>
-                  Читати
-                </button>
-              </Link>
-          </li>
-        ))}
-      </ul>
+                {/* READ BUTTON */}
+                <Link href={`/blog/${article.id}`}>
+                  <button className={styles.btn}>
+                    Читати
+                  </button>
+                </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
