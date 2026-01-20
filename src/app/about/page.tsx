3d75379dd4afb3eb99934533f4a33b7c9fa9d870
Everdarkk <alex.dorokhov.mail@gmail.com>
2026-01-20 09:41:28 +0200

    AboutStaff base added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import AboutHero from "@/components/AboutHero"
 import AboutHistory from "@/components/AboutHistory"
+import AboutStaff from "@/components/AboutStaff"
 
 export default function AboutPage() {
     return (
         <div>
             <AboutHero />
             <AboutHistory />
+            <AboutStaff />
         </div>
     )
 }
---
+++ src/components/AboutStaff.tsx
@@ -0,0 +1,20 @@
+import styles from "../styles/AboutStaff.module.css"
+import { overpass } from "@/app/layout"
+
+export default function AboutStaff() {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                {/* TITLE */}
+                <div className={`${styles.title} ${overpass.className}`}>
+                    <h3>
+                        Наша команда фахівців
+                    </h3>
+
+                    {/* LINE */}
+                    <span className={styles.line}></span>
+                </div>
+            </div>
+        </section>
+    )
+}
